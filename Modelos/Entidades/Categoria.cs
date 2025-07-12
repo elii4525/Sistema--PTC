2dcf2179ff1e383fa2233d31f2d60ed9618cd622
Daniel <20250273@ricaldone.edu.sv>
2025-07-12 17:06:49 -0600

    Entidades

--- Modelos/Entidades/Categoria.cs
+++ Modelos/Entidades/Categoria.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Modelos.Entidades
 {
-    internal class Categoria
+    public class Categoria
     {
     }
 }
--- Modelos/Entidades/Material.cs
+++ Modelos/Entidades/Material.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Modelos.Entidades
 {
-    internal class Material
+    public class Material
     {
     }
 }
