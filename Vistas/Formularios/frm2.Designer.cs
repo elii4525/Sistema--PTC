6b97be24f4cd5cd850769ea287a7a9a08b108cc6
fabi <20220163@gmail.com>
2025-07-21 22:19:28 -0600

    cambios1y2

--- Vistas/Formularios/frm2.Designer.cs
+++ Vistas/Formularios/frm2.Designer.cs
@@ -28,12 +28,261 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm2));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblBienvenido = new System.Windows.Forms.Label();
+            this.lblQuedesehcaer = new System.Windows.Forms.Label();
+            this.btnInventario = new System.Windows.Forms.Button();
+            this.pbLogoIT2 = new System.Windows.Forms.PictureBox();
+            this.btnSolicitudess = new System.Windows.Forms.Button();
+            this.btnConsumo = new System.Windows.Forms.Button();
+            this.lblVer2 = new System.Windows.Forms.Label();
+            this.lblver1 = new System.Windows.Forms.Label();
+            this.lblver3 = new System.Windows.Forms.Label();
+            this.pbinventarioft2 = new System.Windows.Forms.PictureBox();
+            this.pbSoli2 = new System.Windows.Forms.PictureBox();
+            this.pbConsumo2 = new System.Windows.Forms.PictureBox();
+            this.lblArticuloDIT = new System.Windows.Forms.Label();
+            this.lblSolicitudd = new System.Windows.Forms.Label();
+            this.lblComparaciones = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogoIT2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbinventarioft2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSoli2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbConsumo2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.lblComparaciones);
+            this.panel1.Controls.Add(this.lblSolicitudd);
+            this.panel1.Controls.Add(this.lblArticuloDIT);
+            this.panel1.Controls.Add(this.pbConsumo2);
+            this.panel1.Controls.Add(this.pbSoli2);
+            this.panel1.Controls.Add(this.pbinventarioft2);
+            this.panel1.Controls.Add(this.lblver3);
+            this.panel1.Controls.Add(this.lblver1);
+            this.panel1.Controls.Add(this.lblVer2);
+            this.panel1.Controls.Add(this.btnConsumo);
+            this.panel1.Controls.Add(this.btnSolicitudess);
+            this.panel1.Controls.Add(this.pbLogoIT2);
+            this.panel1.Controls.Add(this.btnInventario);
+            this.panel1.Controls.Add(this.lblQuedesehcaer);
+            this.panel1.Controls.Add(this.lblBienvenido);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(47, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(612, 313);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(0, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(611, 35);
+            this.panel2.TabIndex = 0;
+            // 
+            // lblBienvenido
+            // 
+            this.lblBienvenido.AutoSize = true;
+            this.lblBienvenido.Font = new System.Drawing.Font("Segoe UI", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenido.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblBienvenido.Location = new System.Drawing.Point(231, 41);
+            this.lblBienvenido.Name = "lblBienvenido";
+            this.lblBienvenido.Size = new System.Drawing.Size(155, 32);
+            this.lblBienvenido.TabIndex = 1;
+            this.lblBienvenido.Text = "¡Bienvenido!";
+            // 
+            // lblQuedesehcaer
+            // 
+            this.lblQuedesehcaer.AutoSize = true;
+            this.lblQuedesehcaer.Font = new System.Drawing.Font("Segoe UI", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuedesehcaer.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblQuedesehcaer.Location = new System.Drawing.Point(159, 73);
+            this.lblQuedesehcaer.Name = "lblQuedesehcaer";
+            this.lblQuedesehcaer.Size = new System.Drawing.Size(300, 37);
+            this.lblQuedesehcaer.TabIndex = 2;
+            this.lblQuedesehcaer.Text = "¿Qué desea hacer hoy?";
+            // 
+            // btnInventario
+            // 
+            this.btnInventario.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnInventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInventario.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnInventario.Location = new System.Drawing.Point(108, 138);
+            this.btnInventario.Name = "btnInventario";
+            this.btnInventario.Size = new System.Drawing.Size(102, 159);
+            this.btnInventario.TabIndex = 5;
+            this.btnInventario.Text = "Inventario";
+            this.btnInventario.UseVisualStyleBackColor = false;
+            // 
+            // pbLogoIT2
+            // 
+            this.pbLogoIT2.Image = ((System.Drawing.Image)(resources.GetObject("pbLogoIT2.Image")));
+            this.pbLogoIT2.Location = new System.Drawing.Point(2, 238);
+            this.pbLogoIT2.Margin = new System.Windows.Forms.Padding(2);
+            this.pbLogoIT2.Name = "pbLogoIT2";
+            this.pbLogoIT2.Size = new System.Drawing.Size(90, 73);
+            this.pbLogoIT2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbLogoIT2.TabIndex = 6;
+            this.pbLogoIT2.TabStop = false;
+            // 
+            // btnSolicitudess
+            // 
+            this.btnSolicitudess.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSolicitudess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSolicitudess.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSolicitudess.Location = new System.Drawing.Point(247, 138);
+            this.btnSolicitudess.Name = "btnSolicitudess";
+            this.btnSolicitudess.Size = new System.Drawing.Size(106, 159);
+            this.btnSolicitudess.TabIndex = 11;
+            this.btnSolicitudess.Text = "Solicitudes";
+            this.btnSolicitudess.UseVisualStyleBackColor = false;
+            // 
+            // btnConsumo
+            // 
+            this.btnConsumo.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnConsumo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConsumo.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnConsumo.Location = new System.Drawing.Point(393, 138);
+            this.btnConsumo.Name = "btnConsumo";
+            this.btnConsumo.Size = new System.Drawing.Size(102, 159);
+            this.btnConsumo.TabIndex = 12;
+            this.btnConsumo.Text = "Consumo";
+            this.btnConsumo.UseVisualStyleBackColor = false;
+            // 
+            // lblVer2
+            // 
+            this.lblVer2.AutoSize = true;
+            this.lblVer2.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblVer2.Location = new System.Drawing.Point(148, 122);
+            this.lblVer2.Name = "lblVer2";
+            this.lblVer2.Size = new System.Drawing.Size(23, 13);
+            this.lblVer2.TabIndex = 13;
+            this.lblVer2.Text = "Ver";
+            // 
+            // lblver1
+            // 
+            this.lblver1.AutoSize = true;
+            this.lblver1.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblver1.Location = new System.Drawing.Point(288, 122);
+            this.lblver1.Name = "lblver1";
+            this.lblver1.Size = new System.Drawing.Size(23, 13);
+            this.lblver1.TabIndex = 14;
+            this.lblver1.Text = "Ver";
+            // 
+            // lblver3
+            // 
+            this.lblver3.AutoSize = true;
+            this.lblver3.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblver3.Location = new System.Drawing.Point(436, 122);
+            this.lblver3.Name = "lblver3";
+            this.lblver3.Size = new System.Drawing.Size(23, 13);
+            this.lblver3.TabIndex = 15;
+            this.lblver3.Text = "Ver";
+            // 
+            // pbinventarioft2
+            // 
+            this.pbinventarioft2.Image = ((System.Drawing.Image)(resources.GetObject("pbinventarioft2.Image")));
+            this.pbinventarioft2.Location = new System.Drawing.Point(128, 153);
+            this.pbinventarioft2.Name = "pbinventarioft2";
+            this.pbinventarioft2.Size = new System.Drawing.Size(66, 48);
+            this.pbinventarioft2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbinventarioft2.TabIndex = 16;
+            this.pbinventarioft2.TabStop = false;
+            // 
+            // pbSoli2
+            // 
+            this.pbSoli2.Image = ((System.Drawing.Image)(resources.GetObject("pbSoli2.Image")));
+            this.pbSoli2.Location = new System.Drawing.Point(267, 153);
+            this.pbSoli2.Name = "pbSoli2";
+            this.pbSoli2.Size = new System.Drawing.Size(66, 48);
+            this.pbSoli2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSoli2.TabIndex = 17;
+            this.pbSoli2.TabStop = false;
+            // 
+            // pbConsumo2
+            // 
+            this.pbConsumo2.Image = ((System.Drawing.Image)(resources.GetObject("pbConsumo2.Image")));
+            this.pbConsumo2.Location = new System.Drawing.Point(410, 153);
+            this.pbConsumo2.Name = "pbConsumo2";
+            this.pbConsumo2.Size = new System.Drawing.Size(66, 48);
+            this.pbConsumo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbConsumo2.TabIndex = 18;
+            this.pbConsumo2.TabStop = false;
+            // 
+            // lblArticuloDIT
+            // 
+            this.lblArticuloDIT.AutoSize = true;
+            this.lblArticuloDIT.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblArticuloDIT.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblArticuloDIT.Location = new System.Drawing.Point(125, 238);
+            this.lblArticuloDIT.Name = "lblArticuloDIT";
+            this.lblArticuloDIT.Size = new System.Drawing.Size(79, 39);
+            this.lblArticuloDIT.TabIndex = 19;
+            this.lblArticuloDIT.Text = "Articulos en \r\nDIT, actualizar \r\nmateriales.";
+            // 
+            // lblSolicitudd
+            // 
+            this.lblSolicitudd.AutoSize = true;
+            this.lblSolicitudd.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblSolicitudd.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblSolicitudd.Location = new System.Drawing.Point(264, 225);
+            this.lblSolicitudd.Name = "lblSolicitudd";
+            this.lblSolicitudd.Size = new System.Drawing.Size(71, 65);
+            this.lblSolicitudd.TabIndex = 20;
+            this.lblSolicitudd.Text = "Solicitudes a\r\njefatura por\r\nnecesidad de\r\narticulos en\r\nDIT.";
+            // 
+            // lblComparaciones
+            // 
+            this.lblComparaciones.AutoSize = true;
+            this.lblComparaciones.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblComparaciones.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblComparaciones.Location = new System.Drawing.Point(407, 225);
+            this.lblComparaciones.Name = "lblComparaciones";
+            this.lblComparaciones.Size = new System.Drawing.Size(80, 52);
+            this.lblComparaciones.TabIndex = 21;
+            this.lblComparaciones.Text = "Comparacion \r\nde gastos entre\r\nsemanas \r\no meses.";
+            // 
+            // frm2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "frm2";
             this.Text = "frm2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogoIT2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbinventarioft2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSoli2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbConsumo2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblBienvenido;
+        private System.Windows.Forms.Label lblQuedesehcaer;
+        private System.Windows.Forms.Button btnInventario;
+        private System.Windows.Forms.PictureBox pbLogoIT2;
+        private System.Windows.Forms.Button btnSolicitudess;
+        private System.Windows.Forms.Button btnConsumo;
+        private System.Windows.Forms.Label lblver3;
+        private System.Windows.Forms.Label lblver1;
+        private System.Windows.Forms.Label lblVer2;
+        private System.Windows.Forms.PictureBox pbinventarioft2;
+        private System.Windows.Forms.PictureBox pbSoli2;
+        private System.Windows.Forms.PictureBox pbConsumo2;
+        private System.Windows.Forms.Label lblArticuloDIT;
+        private System.Windows.Forms.Label lblSolicitudd;
+        private System.Windows.Forms.Label lblComparaciones;
     }
 }
